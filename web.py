c1e02ce0d2c14d4eacd28bd2e4a6a78267c510bf
sbl <sbl@iguzu.com>
2016-11-16 00:35:14 -0500

    Added flask-alchemy

--- web.py
+++ web.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
 
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run()
