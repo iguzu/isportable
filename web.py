94a4f2a307096bcceee02141796b86931facaee1
sbl <sbl@iguzu.com>
2016-11-16 00:46:18 -0500

    Added flask-alchemy

--- web.py
+++ web.py
@@ -8,11 +8,22 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
 
 @app.route('/')
 def hello_world():
-
-    return render_template('home.html')
+    users = User.query.all()
+    return render_template('home.html',users=users)
 
 if __name__ == '__main__':
     app.run()
