c5dccf2f1fb44ae05a9e486fdb09015582322cd8
sbl <sbl@iguzu.com>
2016-11-22 21:30:52 -0500

    Cleaner importer

--- importer.py
+++ importer.py
@@ -1,9 +1,8 @@
-import logging
 import csv
 import os
-
 from models import RateCenter
 
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 def clear_data(app, db):
@@ -17,26 +16,16 @@ def import_data(app, db):
     with app.app_context():
         if RateCenter.query.count():
             return "Database not empty"
-        APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-        csvfile = open(os.path.join(APP_ROOT,'footprint.csv'))
-        reader = csv.reader(csvfile, delimiter=',',quotechar='"')
+        reader = csv.reader(open(os.path.join(APP_ROOT,'footprint.csv')), delimiter=',',quotechar='"')
         results = list(filter(lambda x: x[4].strip() == 'Y',reader))
         count = 0
         for row in results:
-            state, rate_center, lata, name, coverage = row
-            state = state.strip()
-            rated_center = rate_center.strip()
-            lata = lata.strip()
-            name = name.strip()
-            coverage = coverage.strip()
+            row = [ item.strip() for item in row ]
             rc = RateCenter()
-            rc.state = state
-            rc.rate_center = rate_center
-            rc.name = name
-            rc.lata = lata
-            rc.coverage = True if coverage == 'Y' else False
+            rc.state, rc.rate_center, rc.lata, rc.name, rc.coverage = row
+            rc.coverage = True if rc.coverage == 'Y' else False
             db.session.add(rc)
-            count = count + 1
+            count += 1
             if not count % 100:
                 print('committing... %d RateCenters' % count)
                 db.session.commit()
